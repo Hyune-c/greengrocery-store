f2fe494abb91c04cc162c7f21b74ad66b68cc055
choibyunghyeon <choi8608@gmail.com>
2021-07-10 15:03:14 +0900

    Initial commit

---
+++ src/main/java/com/example/greengrocerystore/GreengroceryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.greengrocerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengroceryStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreengroceryStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greengrocerystore/GreengroceryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greengrocerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreengroceryStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
